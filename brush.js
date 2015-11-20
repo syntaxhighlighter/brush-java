5f23856f4e7f1639f2e5dc9b608d210cb966449f
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-11-20 10:24:21 -0800

    Updates for 2015
    * Removes CoffeeScript
    * Removes Grunt
    * Removes Karma
    * Uses Babel

---
+++ brush.js
@@ -0,0 +1,60 @@
+var BrushBase = require('brush-base');
+var regexLib = require('regex-lib');
+
+function Brush() {
+  var keywords = 'abstract assert boolean break byte case catch char class const ' +
+    'continue default do double else enum extends ' +
+    'false final finally float for goto if implements import ' +
+    'instanceof int interface long native new null ' +
+    'package private protected public return ' +
+    'short static strictfp super switch synchronized this throw throws true ' +
+    'transient try void volatile while';
+
+  this.regexList = [
+    {
+      regex: regexLib.singleLineCComments,
+      css: 'comments'
+    },
+    {
+      regex: /\/\*([^\*][\s\S]*?)?\*\//gm,
+      css: 'comments'
+    },
+    {
+      regex: /\/\*(?!\*\/)\*[\s\S]*?\*\//gm,
+      css: 'preprocessor'
+    },
+    {
+      regex: regexLib.doubleQuotedString,
+      css: 'string'
+    },
+    {
+      regex: regexLib.singleQuotedString,
+      css: 'string'
+    },
+    {
+      regex: /\b([\d]+(\.[\d]+)?|0x[a-f0-9]+)\b/gi,
+      css: 'value'
+    },
+    {
+      regex: /(?!\@interface\b)\@[\$\w]+\b/g,
+      css: 'color1'
+    },
+    {
+      regex: /\@interface\b/g,
+      css: 'color2'
+    },
+    {
+      regex: new RegExp(this.getKeywords(keywords), 'gm'),
+      css: 'keyword'
+    }
+		];
+
+  this.forHtmlScript({
+    left: /(&lt;|<)%[@!=]?/g,
+    right: /%(&gt;|>)/g
+  });
+};
+
+Brush.prototype = new BrushBase();
+Brush.aliases = ['java'];
+module.exports = Brush;
